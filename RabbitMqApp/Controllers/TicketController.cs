c2b52e9676a9312a2b176e034259009b860088b2
unknown <emad_tttt522@hotmail.com>
2020-12-06 18:24:08 +0000

    v1

---
+++ RabbitMqApp/Controllers/TicketController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Model;
+
+namespace RabbitMqApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        //Dont Forget the model name space should be the same in publisher and cosnumer
+        private readonly IBus _bus;
+        public TicketController(IBus bus)
+        {
+            _bus = bus;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateTicket(Ticket ticket)
+        {
+            if (ticket != null)
+            {
+                ticket.BookedOn = DateTime.Now;
+                Uri uri = new Uri("rabbitmq://localhost/ticketQueue");
+                var endPoint = await _bus.GetSendEndpoint(uri);
+                await endPoint.Send(ticket);
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
